32fe26eb15e148aadcb7b3f182e4e2a2d1c1d4c8
DESKTOP-14SNM3A\PATEL <mayur.soboft@gmail.com>
2022-06-08 20:23:05 +0530

    commit set bundle config, layout

--- IRecord-web 02-05-2022 Code/IRecord-web/IRecordweb/App_Start/BundleConfig.cs
+++ IRecord-web 02-05-2022 Code/IRecord-web/IRecordweb/App_Start/BundleConfig.cs
@@ -2,12 +2,12 @@
 using System.Web.Optimization;
 
 namespace IRecordweb
-    {
+{
     public class BundleConfig
-        {
+    {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
-            {
+        {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -21,11 +21,39 @@ namespace IRecordweb
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/js/select2.min.js",
+                      "~/js/jquery-3.5.1.min.js",
                       "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/basescript").Include(
+                "~/Scripts/popper.min.js",
+                "~/Scripts/bootstrap.min.v4.js",
+                "~/js/simplebar.min.js"
+                ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-            }
+            bundles.Add(new ScriptBundle("~/bundles/basescript1").Include(
+               "~/Scripts/metisMenu.min.js",
+               "~/Scripts/app.js"
+               ));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+         "~/AngScripts/angular.js",
+         "~/AngScripts/ngStorage.min.js",
+         "~/AngularDataFunction/AngularDataFunction.js"
+           ));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/basiccss").Include(
+                      "~/Content/bootstrap.min.v4.css",
+                      "~/Content/line-awesome.min.css",
+                      "~/Content/theme-color.css",
+                      "~/Content/app.min.css",
+                      "~/Content/custom-body.css",
+                      "~/Content/themes/base/jquery-ui.min.css",
+                      "~/Content/bootstrap-datetimepicker.min.css",
+                      "~/Content/select2.min.css"
+                      ));
+
         }
     }
+}
